58ffaac8d04b5a0617c434abf2a30cdf9bf275b0
brigere <briantalavera@gmail.com>
2022-07-29 19:50:41 -0300

    added | 404 middlewares

--- src/server.ts
+++ src/server.ts
@@ -22,9 +22,9 @@ export class ExpressApp {
       this.app.use(route.path, route.router)
     })
 
-    this.app.get('/api', (req, res) => res.json({ message: "API" }))
-    this.app.get('/', (req, res) => res.json({ message: "Home" }))
-    this.app.use((req, res, next) => res.status(404).json({message: "Resource does not exist"}))
+    // 404 Error messages
+    this.app.use('/api/', (req, res, next) => res.status(404).json({status: "Error", message: "Resource does not exist, verify the URL"}))
+    this.app.use('/', (req, res, next) => res.status(404).end())
   }
 
   run() {
